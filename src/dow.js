65bd535f911f0511ac33d44164f5ab0cad294f43
[Anthony Cruz] <[anthonyecruz@gmail.com]>
2020-08-18 13:20:21 -0400

    networth line graph rendering

--- src/dow.js
+++ src/dow.js
@@ -1,4 +1,4 @@
-///////////// Networth ////////////
+///////////// Dow jones ////////////
 
 var margin = { top: 20, right: 20, bottom: 30, left: 50 },
   width = 960 - margin.left - margin.right,
@@ -14,26 +14,16 @@ var y = d3.scaleLinear().range([height, 0]);
 // define the 1st line
 var valueline = d3.line()
   .x(function (d) { return x(d.date); })
-  .y(function (d) { return y(d.top1); });
+  .y(function (d) { return y(d.close); });
 
 // define the 2nd line
 var valueline2 = d3.line()
   .x(function (d) { return x(d.date); })
-  .y(function (d) { return y(d.top10); });
-
-// define the 3rd line
-var valueline3 = d3.line()
-  .x(function (d) { return x(d.date); })
-  .y(function (d) { return y(d.top50); });
-
-// define the 3rd line
-var valueline4 = d3.line()
-  .x(function (d) { return x(d.date); })
-  .y(function (d) { return y(d.bottom); });
+  .y(function (d) { return y(d.open); });
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+// moves the 'group' element to the top left 
 var svg = d3.select("body").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -42,20 +32,20 @@ var svg = d3.select("body").append("svg")
     "translate(" + margin.left + "," + margin.top + ")");
 
 // Get the data
-d3.csv("networth.csv", function (error, data) {
+d3.csv("DJI.csv", function (error, data) {
   if (error) throw error;
 
   // format the data
   data.forEach(function (d) {
     d.date = parseTime(d.date);
     d.close = +d.close;
-    d.open = +d.top1;
+    d.open = +d.open;
   });
 
   // Scale the range of the data
   x.domain(d3.extent(data, function (d) { return d.date; }));
   y.domain([0, d3.max(data, function (d) {
-    return Math.max(d.top1, d.top10, d.top50, d.bottom);
+    return Math.max(d.close, d.open);
   })]);
 
   // Add the valueline path.
@@ -65,23 +55,11 @@ d3.csv("networth.csv", function (error, data) {
     .attr("d", valueline);
 
   // Add the valueline2 path.
-  svg.append("path")
-    .data([data])
-    .attr("class", "line")
-    .style("stroke", "cyan")
-    .attr("d", valueline2);
-
-  svg.append("path")
-    .data([data])
-    .attr("class", "line")
-    .style("stroke", "green")
-    .attr("d", valueline3);
-
-  svg.append("path")
-    .data([data])
-    .attr("class", "line")
-    .style("stroke", "red")
-    .attr("d", valueline4);
+  // svg.append("path")
+  //   .data([data])
+  //   .attr("class", "line")
+  //   .style("stroke", "red")
+  //   .attr("d", valueline2);
 
   // Add the X Axis
   svg.append("g")
