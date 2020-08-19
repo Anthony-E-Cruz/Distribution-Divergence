4832823904b97fa105f6c1c1af600af1f30149c3
[Anthony Cruz] <[anthonyecruz@gmail.com]>
2020-08-18 22:41:33 -0400

    modal functional and styled

---
+++ src/modal/modal.js
@@ -0,0 +1,22 @@
+var buttons = document.querySelectorAll('button');
+var modal = document.querySelector('.modal');
+var backdrop = document.querySelector('.backdrop');
+
+var outputParagraph = document.querySelector('#quote');
+var textEdit = document.querySelector('.modal textarea');
+
+var quote = 'dataViz is a';
+
+function closeModal() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+}
+
+buttons[1].addEventListener('click', function () {
+  modal.style.display = 'block';
+  backdrop.style.display = 'block';
+});
+
+buttons[0].addEventListener('click', function () {
+  closeModal();
+});
